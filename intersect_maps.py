9690fd535a0226fc36054272e8699b708d76c137
Evan Sultanik <Evan.Sultanik@digitaloperatives.com>
2017-06-19 22:41:34 -0400

    Got the basics of shapely working

--- intersect_maps.py
+++ intersect_maps.py
@@ -1,68 +1,82 @@
 import itertools
 import json
-from osgeo import ogr
+from shapely.geometry import MultiPolygon, Polygon
 
 class ZoningFeature(object):
-    def __init__(self, zoning, geometry, old_zoning = None):
+    def __init__(self, objectid, zoning, geometry, old_zoning = None):
+        self.objectid = objectid
         self.zoning = zoning
         if old_zoning is None:
             old_zoning = []
         self.old_zoning = old_zoning
         self.geometry = geometry
-    #def __getattr__(self, key):
-    #    return getattr(self._parent_feature, key)
 
 class ZoningMap(object):
-    def __init__(self, path):
-        driver = ogr.GetDriverByName('GeoJSON')
-        datasource = driver.Open(path)
-        self._layer = datasource.GetLayer()
+    def __init__(self, stream):
+        self.json = json.load(stream)
         self._features = []
-        for i in range(len(self._layer)):
-            ogr_feature = self._layer.GetNextFeature()
-            try:
-                zoning = ogr_feature.GetField("LONG_CODE")
-            except Exception:
-                zoning = (ogr_feature.GetField("CODE"), ogr_feature.GetField("CATEGORY"))
-            self._features.append(ZoningFeature([zoning], ogr_feature.GetGeometryRef()))
     def __len__(self):
-        return len(self._features)
-    #    return len(self._layer)
+        return len(self.json["features"])
     def __getitem__(self, key):
-        return self._features[key]
-    #    if key < 0 or key >= len(self):
-    #        return None
-    #    while key >= len(self._features):
-    #        self._features.append(ZoningFeature(self._layer.GetNextFeature()))
-    #    return self._features[key]
+       if key < 0 or key >= len(self):
+           return None
+       while key >= len(self._features):
+           feature = self.json["features"][key]
+           poly = [Polygon(coords) for coords in feature["geometry"]["coordinates"]]
+           if len(poly) == 1:
+               poly = poly[0]
+           else:
+               poly = MultiPolygon(*poly)
+           properties = feature["properties"]
+           if "LONG_CODE" in properties:
+               zoning = properties["LONG_CODE"]
+           else:
+               zoning = (properties["CODE"], properties["CATEGORY"])
+           self._features.append(ZoningFeature(properties["OBJECTID"], [zoning], poly))
+       return self._features[key]
     def __iter__(self):
         for i in range(len(self)):
             yield self[i]
 
 def intersect(map1, map2):
+    intersected = []
+    for f1 in map1:
+        for i in range(len(map2)):
+            pass
+            
+def oldintersect(map1, map2):
     changed = True
-    features1 = [feat for feat in map1]
-    features2 = [feat for feat in map2]
+    iter1 = iter(map1)
+    iter2 = iter(map2)
+    features1 = []
+    features2 = []
     while changed:
         changed = False
-        for i, j in itertools.product(range(len(features1)), range(len(features2))):
+        for i, j in itertools.product(range(len(map1)), range(len(map2))):
+            while i >= len(features1):
+                features1.append(next(iter1))
+            while j >= len(features2):
+                features2.append(next(iter2))
             f1 = features1[i]
             f2 = features2[j]
-            isect = f1.geometry.Intersection(f2.geometry)
-            if not isect.IsEmpty():
+            if f1 is None or f2 is None:
+                continue
+            isect = f1.geometry.intersection(f2.geometry)
+            if not isect.is_empty:
                 changed = True
-                print "GGGG"
-                oldf1 = f1.geometry
-                f1 = ZoningFeature(f1.zoning, f1.geometry.Difference(isect))
-                features1.append(ZoningFeature(f1.zoning, oldf1.Difference(f1.geometry)))
-                oldf2 = f2.geometry
-                f2 = ZoningFeature(f2.zoning, f2.geometry.Difference(isect), f2.old_zoning + f1.zoning)
-                print "Found a plot that went from %s to %s" % (f1.zoning, f2.zoning)
-                features2.append(ZoningFeature(f2.zoning, oldf2.Difference(f2.geometry)))
-                exit(0)
+                features1[i] = ZoningFeature(f1.objectid, f1.zoning, f1.geometry.difference(isect))
+                if features1[i].geometry.area < 1e-09:
+                    features1[i] = None
+                new_portion = ZoningFeature("%s.2" % f1.objectid, f1.zoning, f1.geometry.difference(features1[i].geometry))
+                features1.append()
+                features2[j] = ZoningFeature("%s->%s" % (f1.objectid, f2.objectid), f2.zoning, f2.geometry.difference(isect), f2.old_zoning + f1.zoning)
+                print "Plot %s -> %s went from %s to %s" % (f1.objectid, f2.objectid, f1.zoning, f2.zoning)
+                features2.append(ZoningFeature("%s.2" % f2.objectid, f2.zoning, f2.geometry.difference(features2[j].geometry)))
                 break
-                
+            
 if __name__ == "__main__":
     import sys
 
-    intersect(ZoningMap(sys.argv[1]), ZoningMap(sys.argv[2]))
+    with open(sys.argv[1], 'r') as f1:
+        with open(sys.argv[2], 'r') as f2:
+            intersect(ZoningMap(f1), ZoningMap(f2))

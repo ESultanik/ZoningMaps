bda10098942cac3e178f9ca7e04886c5c02b2125
Evan Sultanik <Evan.Sultanik@digitaloperatives.com>
2017-06-21 06:37:41 -0400

    Cleaned up the time estimation code

--- intersect_maps.py
+++ intersect_maps.py
@@ -1,5 +1,4 @@
-import time
-
+import progress
 import zoning
 
 def intersect(map1, map2, logger = None):
@@ -7,45 +6,19 @@ def intersect(map1, map2, logger = None):
         logger = lambda m : None
     map1 = zoning.ModifiableMap(map1)
     map2 = zoning.ModifiableMap(map2)
-    last_percent = -1
-    last_log_time = 0
-    start_time = time.time()
+    estimator = progress.TimeEstimator(logger, 0, len(map1) * len(map2), precision = 2, interval = 3.0)
     for n, f1 in enumerate(map1):
         if f1.geometry.is_empty:
             continue
         for i, f2 in enumerate(map2):
-            raw_percent = float((n * len(map2) + i) * 10000) / float(len(map1) * len(map2))
-            percent = float(int(raw_percent)) / 100.0
-            raw_percent /= 100.0
-            current_time = time.time()
-            if percent > last_percent or current_time - last_log_time >= 3:
-                if raw_percent == 0:
-                    time_remaining = "????"
-                else:
-                    seconds_remaining = (current_time - start_time) / raw_percent * (100.0 - raw_percent)
-                    time_remaining = ""
-                    if seconds_remaining >= 60**2:
-                        hours = int(seconds_remaining / 60**2)
-                        time_remaining += "%d:" % hours
-                        seconds_remaining -= hours * 60**2
-                    if seconds_remaining >= 60 or time_remaining:
-                        minutes = int(seconds_remaining / 60)
-                        time_remaining += "%02d:" % minutes
-                        seconds_remaining -= minutes * 60
-                    if not time_remaining:
-                        time_remaining = "%.2f seconds" % seconds_remaining
-                    else:
-                        time_remaining += "%02d" % int(seconds_remaining)
-                logger("\r%s\r%.2f%% %s remaining" % (' ' * 40, percent, time_remaining))
-                last_percent = percent
-                last_log_time = current_time
+            estimator.update(n * len(map2) + i)
             if f2.geometry.is_empty:
                 continue
             try:
                 isect = f1.geometry.intersection(f2.geometry)
             except Exception as e:
                 logger("\r%s\rError: %s\n" % (' ' * 40, e))
-                last_percent = -1
+                estimator.force_next_refresh()
                 continue
             if isect.is_empty:
                 continue
@@ -65,7 +38,7 @@ def intersect(map1, map2, logger = None):
                     map2.append(zoning.ZoningFeature("%s.2" % f2.objectid, f2.zoning, new_geom, f2.old_zoning))
             map2[i] = new_feature
             logger("\r%s\rPlot %s (%.02f acres) -> %s (%.02f acres) went from %s to %s\n" % (' ' * 40, f1.objectid, zoning.square_meters_to_acres(f1.area()), f2.objectid, zoning.square_meters_to_acres(new_feature.area()), f1.zoning, f2.zoning))
-            last_percent = -1
+            estimator.force_next_refresh()
             # Delete the portion of overlap in f1 to hopefully speed up further comparisons:
             # (This is making the assumption that the zoning regions in map2 are non-overlapping)
             map1[n] = zoning.ZoningFeature(f1.objectid, f1.zoning, f1.geometry.difference(isect))
---
+++ progress.py
@@ -0,0 +1,67 @@
+import sys
+import time
+
+class Progress(object):
+    def __init__(self, time, percent):
+        self.time = time
+        self.percent = percent
+
+def default_logger(msg):
+    sys.stderr.write(msg)
+    sys.stderr.flush()
+        
+class TimeEstimator(object):
+    def __init__(self, logger = None, start_value = 0.0, end_value = 100.0, precision = 2, interval = 3.0, window = None):
+        self.value = start_value
+        self.end_value = end_value
+        self.progress = []
+        if logger is None:
+            logger = default_logger
+        self.logger = logger
+        self.interval = interval
+        self.precision = precision
+        if window is None:
+            window = interval * 4
+        self.window = window
+        self.last_log_time = 0
+        self.last_percent = -1
+        self.start_time = time.time()
+    def increment(self, increment = 1):
+        self.value += increment
+        self.refresh()
+    def update(self, new_value):
+        self.value = new_value
+        self.refresh()
+    def force_next_refresh(self):
+        self.last_percent = -1
+    def refresh(self):
+        raw_percent = float((self.value) * 10**(2+self.precision)) / float(self.end_value)
+        percent = float(int(raw_percent)) / 10**self.precision
+        raw_percent /= 10**self.precision
+        current_time = time.time()
+        self.progress.append(Progress(current_time, raw_percent))
+        if percent > self.last_percent or not self.progress or current_time - self.last_log_time >= self.interval:
+            self.last_log_time = current_time
+            self.last_percent = percent
+            prune_index = 0
+            while len(self.progress) - prune_index > 2 and current_time - self.progress[prune_index].time > self.window:
+                prune_index += 1
+            self.progress = self.progress[prune_index:]
+            if raw_percent == 0 or len(self.progress) < 2 or self.progress[-1].percent == self.progress[0].percent:
+                time_remaining = "????"
+            else:
+                seconds_remaining = (current_time - self.progress[0].time) / (self.progress[-1].percent - self.progress[0].percent) * (100.0 - raw_percent)
+                time_remaining = ""
+                if seconds_remaining >= 60**2:
+                    hours = int(seconds_remaining / 60**2)
+                    time_remaining += "%d:" % hours
+                    seconds_remaining -= hours * 60**2
+                if seconds_remaining >= 60 or time_remaining:
+                    minutes = int(seconds_remaining / 60)
+                    time_remaining += "%02d:" % minutes
+                    seconds_remaining -= minutes * 60
+                if not time_remaining:
+                    time_remaining = "%.2f seconds" % seconds_remaining
+                else:
+                    time_remaining += "%02d" % int(seconds_remaining)
+            self.logger("\r%s\r%.2f%% %s remaining" % (' ' * 40, percent, time_remaining))

efc647fd6648140799e3081c1998092135b6732b
Evan Sultanik <evan@sultanik.com>
2017-06-22 15:28:42 -0400

    Generic zoning coloring

--- mapping.py
+++ mapping.py
@@ -10,8 +10,19 @@ def map_to_kml(zoning_map):
     k.append(d)
     f = kml.Folder(ns, 'PHL Zoning', 'Philadelphia Zoning', 'Changes to Philadelphia zoning from 2012 to 2017')
     d.append(f)
-    rsa = fastkml.styles.Style(ns=ns, styles=[fastkml.styles.PolyStyle(ns=ns, color="7f00ff00", fill=1, outline=1)])
-    assert isinstance(rsa, fastkml.styles._StyleSelector)
+    zoning_colors = {
+        ("RSD-1", "RSD-2", "RSD-3") : (245, 240, 192),
+        ("RSA-1", "RSA-2", "RSA-3") : (244, 239, 103),
+        ("RSA-4", "RSA-5") : (240, 235, 101),
+        ("RTA-1",) : (205, 181, 78),
+        ("RM-1", "RM-2", "RM-3") : (252, 184, 80),
+        }
+    zoning_styles = {}
+    for classes, rgb in zoning_colors.iteritems():
+        hexstyle = "7f%02x%02x%02x" % tuple(reversed(rgb))
+        style = fastkml.styles.Style(ns=ns, styles=[fastkml.styles.PolyStyle(ns=ns, color=hexstyle, fill=1, outline=1)])
+        for c in classes:
+            zoning_styles[c] = style
     for feature in zoning_map:
         if feature.geometry.geom_type == "Polygon":
             #polygons = [feature]
@@ -29,8 +40,9 @@ def map_to_kml(zoning_map):
         zoning = feature.zoning
         while type(zoning) == list and len(zoning) == 1:
             zoning = zoning[0]
-        if str(zoning).startswith("RSA"):
-            p.append_style(rsa)
+        zoning = str(zoning)
+        if zoning in zoning_styles:
+            p.append_style(zoning_styles[zoning])
         p.geometry = feature.geometry
         f.append(p)
     return k

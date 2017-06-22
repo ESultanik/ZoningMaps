1bdc2c114be30472ac08e2b5afd386af122e01d4
Evan Sultanik <evan@sultanik.com>
2017-06-22 15:56:21 -0400

    Got the interactive KML working

--- mapping.py
+++ mapping.py
@@ -1,6 +1,7 @@
 from fastkml import kml
 import fastkml
 
+import shapely
 import zoning
 
 def map_to_kml(zoning_map):
@@ -15,7 +16,22 @@ def map_to_kml(zoning_map):
         ("RSA-1", "RSA-2", "RSA-3") : (244, 239, 103),
         ("RSA-4", "RSA-5") : (240, 235, 101),
         ("RTA-1",) : (205, 181, 78),
-        ("RM-1", "RM-2", "RM-3") : (252, 184, 80),
+        ("RM-%d" % i for i in range(1,5)) : (252, 184, 80),
+        ("RMX-1", "RMX-2", "RMX-3") : (230, 132, 37),
+        ("CMX-1", "CMX-2", "CMX-2.5") : (241, 102, 103),
+        ("CMX-3", "CMX-4") : (238, 33, 35),
+        ("CMX-5",) : (177, 31, 36),
+        ("CA-1", "CA-2") : (247, 161, 163),
+        ("IRMX",) : (212, 165, 120),
+        ("ICMX",) : (220, 190, 218),
+        ("I-1",) : (178, 127, 183),
+        ("I-2",) : (135, 74, 157),
+        ("I-3",) : (84, 71, 157),
+        ("I-P",) : (129, 126, 188),
+        ("SP-PO-A", "SP-PO-P") : (0, 255, 0),
+        ("SP-INS",) : (235, 232, 197),
+        ("SP-ENT",) : (235, 0, 0),
+        ("SP-STA",) : (255, 0, 0),
         }
     zoning_styles = {}
     for classes, rgb in zoning_colors.iteritems():
@@ -25,26 +41,24 @@ def map_to_kml(zoning_map):
             zoning_styles[c] = style
     for feature in zoning_map:
         if feature.geometry.geom_type == "Polygon":
-            #polygons = [feature]
-            pass
+            polygons = [feature.geometry]
         else:
-            continue
-            #allparts = [p.buffer(0) for p in feature.geometry]
-            #poly.geometry = shapely.ops.cascaded_union(allparts)
-            #x, y = poly.geometry.exterior.xy  # here happens the error
-
-            #import sys
-            #sys.stderr.write(feature.geometry.geom_type + "\n")
-            #exit(1)
-        p = kml.Placemark(ns, str(feature.objectid), str(feature.zoning), "Pre-2012 Zoning: %s" % (', '.join(map(str, feature.old_zoning))))
+            allparts = [p.buffer(0) for p in feature.geometry]
+            polygons = [shapely.ops.cascaded_union(allparts)]
         zoning = feature.zoning
         while type(zoning) == list and len(zoning) == 1:
             zoning = zoning[0]
         zoning = str(zoning)
-        if zoning in zoning_styles:
-            p.append_style(zoning_styles[zoning])
-        p.geometry = feature.geometry
-        f.append(p)
+        if feature.old_zoning:
+            old_zoning = " and ".join(map(lambda z : ' '.join(z), feature.old_zoning))
+        else:
+            old_zoning = "N/A"
+        for poly in polygons:
+            p = kml.Placemark(ns, str(feature.objectid), zoning, "Pre-2012 Zoning: %s" % old_zoning)
+            if zoning in zoning_styles:
+                p.append_style(zoning_styles[zoning])
+            p.geometry = poly
+            f.append(p)
     return k
 #def make_zoning_kml():
 
